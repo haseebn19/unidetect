fb37425f71308b86f4ee24277de60906b0b3685a
haseebn19 <haseeb.kn@outlook.com>
2025-09-27 01:32:55 -0400

    Small code tweak

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, memo, useEffect, useRef} from 'react';
+import React, {memo, useEffect, useRef} from 'react';
 import {MessageType} from '../../types';
 import './FileUpload.css';
 
